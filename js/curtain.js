aa850182448fa91f59d4d30779f54dc91f1eef01
mystic <148622715+lomlja@users.noreply.github.com>
2024-02-27 22:54:34 +0400

    glowing when enter and pass change for gallery

--- js/curtain.js
+++ js/curtain.js
@@ -1,9 +1,22 @@
-document.getElementById('triggerDiv').addEventListener('click', function () {
-  document.getElementById('popup').style.display = 'flex';
-});
+document.addEventListener("DOMContentLoaded", function() {
+  const sendMsgDiv = document.getElementById('sendMsg');
+  const msgTxtInput = document.getElementById('msgTxt');
 
-document.getElementById('popup').addEventListener('click', function (event) {
-  if (event.target === this) {
-    this.style.display = 'none';
-  }
+  document.addEventListener("keydown", function(event) {
+      if (event.key === "Enter") {
+          sendMsgDiv.classList.add('active');
+          setTimeout(() => {
+              sendMsgDiv.classList.remove('active');
+          }, 500);
+      }
+  });
+
+  msgTxtInput.addEventListener("input", function(event) {
+      const msgBtn = document.getElementById('msgBtn');
+      if (msgTxtInput.value.trim() !== "") {
+          msgBtn.removeAttribute("disabled");
+      } else {
+          msgBtn.setAttribute("disabled", "disabled");
+      }
+  });
 });
